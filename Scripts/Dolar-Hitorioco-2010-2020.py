37eb384466b072918256402d956d534168186b7b
Pedro Ghirardi <pedroghirardi49@gmail.com>
2022-11-08 00:10:31 -0300

    Actualizacion del dolar historico

--- Scripts/Dolar-Hitorioco-2010-2020.py
+++ Scripts/Dolar-Hitorioco-2010-2020.py
@@ -7,7 +7,7 @@ variaciones_diccionary = []
 
 
 # Años a consultar / solo disponibe desde 2002 hasta 2020
-years = [2010, 2011, 2012, 2013, 2014, 2015, 2016, 2017, 2018, 2019]
+years = [2005,2006,2007,2008,2009,2010, 2011, 2012, 2013, 2014, 2015, 2016, 2017, 2018, 2019]
 
 meses = ["enero","febrero","marzo","abril","mayo","junio","julio","agosto","saptiembre","octubre","nobiembre","diciembre"]
 
